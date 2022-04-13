cddf7afea7d54cee3257f4544d7c54cfaa9e1501
poiuty123 <7975217+poiuty123@user.noreply.gitee.com>
2022-04-13 15:11:46 +0000

    update x-ui/x-ui-ish-new.sh.

--- x-ui/x-ui-ish-new.sh
+++ x-ui/x-ui-ish-new.sh
@@ -49,7 +49,7 @@ case "$(arch -s)" in
         MACHINE='s390x'
         ;;
     *)
-        echo "error: The architecture is not supported."
+        echo "error: 不支持的架构"
         exit 1
         ;;
 esac
@@ -59,46 +59,33 @@ ZIP_FILE="${TMP_DIRECTORY}Xray-linux-$MACHINE.zip"
 DOWNLOAD_LINK="https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-$MACHINE.zip"
 
 install_software() {
-    if [[ -n "$(command -v curl)" ]]; then
+    if [[ -n "$(command -v wget)" ]]; then
         return
     fi
     if [[ -n "$(command -v unzip)" ]]; then
         return
     fi
     if [ "$(command -v apk)" ]; then
-        apk add curl unzip
+        apk add wget unzip -y
     else
-        echo "error: The script does not support the package manager in this operating system."
+        echo "error: 请手动安装."
         exit 1
     fi
 }
 
-download_xray() {
-    curl -L -H 'Cache-Control: no-cache' -o "$ZIP_FILE" "$DOWNLOAD_LINK" -#
-    if [ "$?" -ne '0' ]; then
-        echo 'error: Download failed! Please check your network or try again.'
-        exit 1
-    fi
-    curl -L -H 'Cache-Control: no-cache' -o "$ZIP_FILE.dgst" "$DOWNLOAD_LINK.dgst" -#
-    if [ "$?" -ne '0' ]; then
-        echo 'error: Download failed! Please check your network or try again.'
-        exit 1
-    fi
-}
-
-verification_xray() {
-    for LISTSUM in 'md5' 'sha1' 'sha256' 'sha512'; do
-        SUM="$(${LISTSUM}sum $ZIP_FILE | sed 's/ .*//')"
-        CHECKSUM="$(grep $(echo $LISTSUM | tr [:lower:] [:upper:]) $ZIP_FILE.dgst | uniq | sed 's/.* //')"
-        if [ "$SUM" != "$CHECKSUM" ]; then
-            echo 'error: Check failed! Please check your network or try again.'
-            exit 1
-        fi
-    done
-}
-
-decompression() {
-    unzip -q "$ZIP_FILE" -d "$TMP_DIRECTORY"
+install_xray() {
+    wget -N --no-check-certificate -O ${XRAY_FILE} ${DOWNLOAD_XRAY_LINK}
+    unzip -q ${XRAY_FILE} -d ${TMP_DIRECTORY}
+    install -d /usr/local/xray/
+    install -m 755 "${TMP_DIRECTORY}xray" "/usr/local/xray/xray"
+    install -m 755 "${TMP_DIRECTORY}geoip.dat" "/usr/local/xray/geoip.dat"
+    install -m 755 "${TMP_DIRECTORY}geosite.dat" "/usr/local/xray/geosite.dat"
+    wget /usr/local/xray https://gitee.com/poiuty123/all/raw/master/x-ui/config.json
+    chmod +x /usr/local/xray/config.json
+    OPENRC='0'
+    wget /etc/init.d/xray https://gitee.com/poiuty123/all/raw/master/x-ui/init.d
+    OPENRC='1'
+    
 }
 
 is_it_running() {
@@ -109,93 +96,11 @@ is_it_running() {
     fi
 }
 
-install_xray() {
-    install -m 755 "${TMP_DIRECTORY}xray" "/usr/local/bin/xray"
-    install -d /usr/local/lib/xray/
-    install -m 755 "${TMP_DIRECTORY}geoip.dat" "/usr/local/lib/xray/geoip.dat"
-    install -m 755 "${TMP_DIRECTORY}geosite.dat" "/usr/local/lib/xray/geosite.dat"
-}
-
-install_confdir() {
-    CONFDIR='0'
-    if [ ! -d '/usr/local/etc/xray/' ]; then
-        install -d /usr/local/etc/xray/
-        for BASE in 00_log 01_api 02_dns 03_routing 04_policy 05_inbounds 06_outbounds 07_transport 08_stats 09_reverse; do
-            echo '{}' > "/usr/local/etc/xray/$BASE.json"
-        done
-        CONFDIR='1'
-    fi
-}
-
-install_log() {
-    LOG='0'
-    if [ ! -d '/var/log/xray/' ]; then
-        install -d -o nobody -g nobody /var/log/xray/
-        install -m 600 -o nobody -g nobody /dev/null /var/log/xray/access.log
-        install -m 600 -o nobody -g nobody /dev/null /var/log/xray/error.log
-        LOG='1'
-    fi
-}
-
-install_startup_service_file() {
-    OPENRC='0'
-    if [ ! -f '/etc/init.d/xray' ]; then
-        mkdir "${TMP_DIRECTORY}init.d/"
-        curl -o "${TMP_DIRECTORY}init.d/xray" https://raw.githubusercontent.com/XTLS/alpinelinux-install-xray/main/init.d/xray -s
-        if [ "$?" -ne '0' ]; then
-            echo 'error: Failed to start service file download! Please check your network or try again.'
-            exit 1
-        fi
-        install -m 755 "${TMP_DIRECTORY}init.d/xray" /etc/init.d/xray
-        OPENRC='1'
-    fi
-}
-
-information() {
-    echo 'installed: /usr/local/bin/xray'
-    echo 'installed: /usr/local/lib/xray/geoip.dat'
-    echo 'installed: /usr/local/lib/xray/geosite.dat'
-    if [ "$CONFDIR" -eq '1' ]; then
-        echo 'installed: /usr/local/etc/xray/00_log.json'
-        echo 'installed: /usr/local/etc/xray/01_api.json'
-        echo 'installed: /usr/local/etc/xray/02_dns.json'
-        echo 'installed: /usr/local/etc/xray/03_routing.json'
-        echo 'installed: /usr/local/etc/xray/04_policy.json'
-        echo 'installed: /usr/local/etc/xray/05_inbounds.json'
-        echo 'installed: /usr/local/etc/xray/06_outbounds.json'
-        echo 'installed: /usr/local/etc/xray/07_transport.json'
-        echo 'installed: /usr/local/etc/xray/08_stats.json'
-        echo 'installed: /usr/local/etc/xray/09_reverse.json'
-    fi
-    if [ "$LOG" -eq '1' ]; then
-        echo 'installed: /var/log/xray/'
-    fi
-    if [ "$OPENRC" -eq '1' ]; then
-        echo 'installed: /etc/init.d/xray'
-    fi
-    rm -r "$TMP_DIRECTORY"
-    echo "removed: $TMP_DIRECTORY"
-    echo "You may need to execute a command to remove dependent software: apk del curl unzip"
-    if [ "$XRAY_RUNNING" -eq '1' ]; then
-        rc-service xray start
-    else
-        echo 'Please execute the command: rc-update add xray; rc-service xray start'
-    fi
-    echo "info: Xray is installed."
-}
 
 main() {
     install_software
-    install_software
-    download_xray
-    verification_xray
-    decompression
     is_it_running
     install_xray
-    install_confdir
-    install_log
-    install_startup_service_file
-    information
 }
 
 main
