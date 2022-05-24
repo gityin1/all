3687b59821f421ac03d724fb1402e45322afb21d
ppoonk <101167225+ppoonk@users.noreply.github.com>
2022-05-24 15:13:19 +0800

    Update ponk-tools.sh

--- ponk-tools.sh
+++ ponk-tools.sh
@@ -93,11 +93,10 @@ check_enabled() {
 # check root
 [[ $EUID -ne 0 ]] && echo -e red "错误：必须使用root用户运行此脚本！\n" && exit 1
 # 下载脚本
-Add_Shell() {
-    wget -O /usr/bin/ponk -N --no-check-certificate https://raw.githubusercontent.com/ppoonk/all/master/ponk-tools.sh
-    chmod +x /usr/bin/ponk
-   
-}
+#Add_Shell() {
+#   wget -O /usr/bin/ponk -N --no-check-certificate https://raw.githubusercontent.com/ppoonk/all/master/ponk-tools.sh
+#   chmod +x /usr/bin/ponk
+#   }
 
 # 返回主菜单
 Return_Show_Menu() {
@@ -111,60 +110,6 @@ Show_Information() {
     echo -e "OS:$(blue "$OS" $lbit位)   Arch：$(blue "$arch")   虚拟化：$(blue "$virt")"   
     echo -e ""
     blue "————————————————————————————————————————————————————————————————————————————————————————————————————————"
-}
-# 菜单
-Show_Menu() {
-    clear
-    clear
-    Show_Information
-    yellow "直接在命令行输入ponk即可运行本脚本。请输入需要执行的序号："
-    echo -e ""
-    yellow "11.国内机更换github hosts"
-    yellow "12.bench.sh"          
-    yellow "13.三网测速 "           
-    yellow "14.路由回程测试"             
-    yellow "15.warp"
-    echo -e ""
-    yellow "21.x-ui"
-    yellow "22.mack-a八合一脚本"
-    yellow "23.CloudFlare Argo Tunnel隧道"
-    yellow "24.BBR"
-    yellow "25.BBr for openvz"
-    yellow "26.下载XrayR"
-    echo -e ""
-    yellow "31.ServerStatus-Hotaru服务端"
-    yellow "32.ServerStatus-Hotaru客户端"
-    echo -e ""
-    yellow "41.安装宝塔770"
-    yellow "42.将xxx写入systemctl服务"
-    echo -e ""
-    yellow "52.卸载apache2"
-    yellow "0.回车或输入0退出"
-    echo -e ""
-    read -p "请输入脚本序号:" Input
-    
-    case "$Input" in
-        0) exit 0 ;;
-        11) sed -i "/# GitHub520 Host Start/Q" /etc/hosts && curl https://raw.hellogithub.com/hosts >> /etc/hosts ;;
-        12) curl -so- 86.re/bench.sh | bash ;;
-        13) bash <(curl -Lso- https://git.io/superspeed);;
-        14) wget https://raw.githubusercontent.com/nanqinlang-script/testrace/master/testrace.sh && bash testrace.sh;;
-        15) wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh ;;
-        21) bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh) ;;
-        22) wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/install.sh" && chmod 700 /root/install.sh && /root/install.sh ;;
-        23) wget -N https://raw.githubusercontents.com/Misaka-blog/argo-tunnel-script/master/argo.sh && bash argo.sh ;;
-        24) wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh ;;
-        25) wget --no-cache -O lkl-haproxy.sh https://github.com/mzz2017/lkl-haproxy/raw/master/lkl-haproxy.sh && bash lkl-haproxy.sh ;;
-        26) wget -O xrayr.zip https://github.com/Misaka-blog/XrayR/releases/latest/download/XrayR-linux-64.zip && unzip -d ./xrayr xrayr.zip ;;
-        31) wget https://raw.githubusercontent.com/cokemine/ServerStatus-Hotaru/master/status.sh && bash status.sh s ;;
-        32) wget https://raw.githubusercontent.com/cokemine/ServerStatus-Hotaru/master/status.sh && bash status.sh c ;;
-        41) Install_BT;;
-        42) Add_Systemctl_Service ;;
-        51) curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun ;;
-        52) uninstall_apache2 ;;
-        
-    esac
-
 }
 
 # 添加服务
@@ -215,5 +160,159 @@ uninstall_apache2() {
        # fi
     fi
 }
-Add_Shell
+
+Install_xui_cn() {
+
+arch=$(arch)
+
+if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
+    arch="amd64"
+elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+    arch="arm64"
+elif [[ $arch == "s390x" ]]; then
+    arch="s390x"
+else
+    arch="amd64"
+    red "检测架构失败，使用默认架构: ${arch}"
+fi
+
+echo "架构: ${arch}"
+
+if [ $(getconf WORD_BIT) != '32' ] && [ $(getconf LONG_BIT) != '64' ]; then
+    echo "本软件不支持 32 位系统(x86)，请使用 64 位系统(x86_64)，如果检测有误，请联系作者"
+    exit -1
+fi
+
+install_base() {
+    if [[ x"$Cmd_Type" == x"centos" ]]; then
+        yum install wget curl tar -y
+    else
+        apt install wget curl tar -y
+    fi
+}
+
+#This function will be called when user installed x-ui out of sercurity
+config_after_install() {
+    yellow "出于安全考虑，安装完成后需要强制修改端口与账户密码"
+    read -p "请设置您的账户名:" config_account
+    yellow "您的账户名将设定为:${config_account}"
+    read -p "请设置您的账户密码:" config_password
+    yellow "您的账户密码将设定为:${config_password}$"
+    read -p "请设置面板访问端口:" config_port
+    yellow "您的面板访问端口将设定为:${config_port}"
+    read -p "确认设定完成？[y/n]": config_confirm
+    if [[ x"${config_confirm}" == x"y" || x"${config_confirm}" == x"Y" ]]; then
+        yellow "确认设定,设定中"
+        /usr/local/x-ui/x-ui setting -username ${config_account} -password ${config_password}
+        yellow "账户密码设定完成"
+        /usr/local/x-ui/x-ui setting -port ${config_port}
+        yellow "面板端口设定完成"
+    else
+        red "已取消,所有设置项均为默认设置,请及时修改"
+    fi
+}
+
+install_x-ui() {
+    systemctl stop x-ui
+    cd /usr/local/
+    url="https://download.fastgit.org/vaxilu/x-ui/releases/latest/download/x-ui-linux-${arch}.tar.gz"
+    echo -e "开始安装 x-ui v"
+    wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url}
+    if [[ -e /usr/local/x-ui/ ]]; then
+        rm /usr/local/x-ui/ -rf
+    fi
+    tar zxvf x-ui-linux-${arch}.tar.gz
+    rm x-ui-linux-${arch}.tar.gz -f
+    cd x-ui
+    chmod +x x-ui bin/xray-linux-${arch}
+    cp -f x-ui.service /etc/systemd/system/
+    wget --no-check-certificate -O /usr/bin/x-ui https://raw.fastgit.org/vaxilu/x-ui/main/x-ui.sh
+    chmod +x /usr/local/x-ui/x-ui.sh
+    chmod +x /usr/bin/x-ui
+    config_after_install
+
+    systemctl daemon-reload
+    systemctl enable x-ui
+    systemctl start x-ui
+    echo -e "x-ui 安装完成，面板已启动，"
+    echo -e ""
+    echo -e "x-ui 管理脚本使用方法: "
+    echo -e "----------------------------------------------"
+    echo -e "x-ui              - 显示管理菜单 (功能更多)"
+    echo -e "x-ui start        - 启动 x-ui 面板"
+    echo -e "x-ui stop         - 停止 x-ui 面板"
+    echo -e "x-ui restart      - 重启 x-ui 面板"
+    echo -e "x-ui status       - 查看 x-ui 状态"
+    echo -e "x-ui enable       - 设置 x-ui 开机自启"
+    echo -e "x-ui disable      - 取消 x-ui 开机自启"
+    echo -e "x-ui log          - 查看 x-ui 日志"
+    echo -e "x-ui v2-ui        - 迁移本机器的 v2-ui 账号数据至 x-ui"
+    echo -e "x-ui update       - 更新 x-ui 面板"
+    echo -e "x-ui install      - 安装 x-ui 面板"
+    echo -e "x-ui uninstall    - 卸载 x-ui 面板"
+    echo -e "----------------------------------------------"
+}
+
+yellow "开始安装"
+install_base
+install_x-ui
+}
+
+# 菜单
+Show_Menu() {
+    clear
+    clear
+#   Show_Information
+    yellow "直接在命令行输入ponk即可运行本脚本。请输入需要执行的序号："
+    echo -e ""
+    yellow "11.国内机更换github hosts"
+    yellow "12.bench.sh"          
+    yellow "13.三网测速 "           
+    yellow "4.路由回程测试"             
+    yellow "15.warp"
+    echo -e ""
+    yellow "21.x-ui"
+    yellow "22.x-ui国内机适用"
+    yellow "23.mack-a八合一脚本"
+    yellow "24.CloudFlare Argo Tunnel隧道"
+    yellow "25.BBR"
+    yellow "26.BBr for openvz"
+    yellow "27.下载XrayR"
+    echo -e ""
+    yellow "31.ServerStatus-Hotaru服务端"
+    yellow "32.ServerStatus-Hotaru客户端"
+    echo -e ""
+    yellow "41.安装宝塔770"
+    yellow "42.将xxx写入systemctl服务"
+    echo -e ""
+    yellow "52.卸载apache2"
+    yellow "0.回车或输入0退出"
+    echo -e ""
+    read -p "请输入脚本序号:" Input
+    
+    case "$Input" in
+        0) exit 0 ;;
+        11) sed -i "/# GitHub520 Host Start/Q" /etc/hosts && curl https://raw.hellogithub.com/hosts >> /etc/hosts ;;
+        12) curl -so- 86.re/bench.sh | bash ;;
+        13) bash <(curl -Lso- https://git.io/Jlkmw);;
+        14) wget https://raw.githubusercontent.com/nanqinlang-script/testrace/master/testrace.sh && bash testrace.sh;;
+        15) wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh ;;
+        21) bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh) ;;
+        22) Install_xui_cn ;;
+        23) wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/install.sh" && chmod 700 /root/install.sh && /root/install.sh ;;
+        24) wget -N https://raw.githubusercontents.com/Misaka-blog/argo-tunnel-script/master/argo.sh && bash argo.sh ;;
+        25) wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh ;;
+        26) wget --no-cache -O lkl-haproxy.sh https://github.com/mzz2017/lkl-haproxy/raw/master/lkl-haproxy.sh && bash lkl-haproxy.sh ;;
+        27) wget -O xrayr.zip https://github.com/Misaka-blog/XrayR/releases/latest/download/XrayR-linux-64.zip && unzip -d ./xrayr xrayr.zip ;;
+        31) wget https://raw.githubusercontent.com/cokemine/ServerStatus-Hotaru/master/status.sh && bash status.sh s ;;
+        32) wget https://raw.githubusercontent.com/cokemine/ServerStatus-Hotaru/master/status.sh && bash status.sh c ;;
+        41) Install_BT;;
+        42) Add_Systemctl_Service ;;
+        51) curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun ;;
+        52) uninstall_apache2 ;;
+        
+    esac
+
+}
+# Add_Shell
 Show_Menu
