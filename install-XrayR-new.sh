62dc5f34232788e2679e8bebe18e308aa04b0f73
ppoonk <101167225+ppoonk@users.noreply.github.com>
2022-08-25 12:21:59 +0800

    Update install-XrayR-new.sh

--- install-XrayR-new.sh
+++ install-XrayR-new.sh
@@ -62,9 +62,11 @@ fi
 
 install_base() {
     if [[ x"${release}" == x"centos" ]]; then
+        yum update -y
         yum install epel-release -y
         yum install wget curl unzip tar crontabs socat -y
     else
+        apt update -y
         apt install wget curl unzip tar cron socat -y
     fi
 }
