b1ad7b8d3f04a75db5167719042b1ecd0542ae34
ppoonk <101167225+ppoonk@users.noreply.github.com>
2022-11-17 20:50:04 +0800

    Create x-ui-series.sh

--- x-ui-series.sh
+++ x-ui-series.sh
@@ -185,6 +185,94 @@ Install_xui_ponk() {
     }
 #安装x-ui for deploy
 Deploy_x-ui() {
+    rm -rf /usr/bin/x-ui
+    cat > /usr/bin/x-ui << EOF
+ #!/bin/bash
+
+green() {
+	echo -e "\033[32m\033[01m$1\033[0m"
+    }
+
+red() {
+	echo -e "\033[31m\033[01m$1\033[0m"
+    }
+
+yellow() {
+	echo -e "\033[33m\033[01m$1\033[0m"
+    }
+
+blue() {
+    echo -e "\033[36m\033[01m$1\033[0m"
+}
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+
+status1="未安装"
+status2="未运行"
+    if [[ -f /usr/bin/x-ui ]]; then
+        status1="已安装"
+    fi
+    temp=$(systemctl status x-ui | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+    if [[ x"${temp}" == x"running" ]]; then
+        status2="已运行"
+    fi
+
+
+ Start_deploy_x-ui() {
+
+        cd /usr/local/x-ui
+       # /usr/local/x-ui/x-ui setting -username admin -password admin
+        screen -USdm x-ui ./x-ui
+ 
+       
+        yellow "x-ui已启动，访问IP:端口  即可管理xui面板"
+        yellow "命令行输入 x-ui 回车，可进行重启，卸载，启动等操作"
+    }
+    Stop_deploy_x-ui() {
+        screen -S x-ui -X quit
+        xuistatus=$(ps -ef | grep "x-ui" | grep -v "grep" | awk '{print $2}')
+        kill -9 $xuistatus
+        yellow "x-ui已停止"
+    }
+
+    Uninstall_deploy_x-ui() {
+        Stop_deploy_x-ui
+        yellow "正在卸载..."
+        rm -rf /usr/local/x-ui
+        yellow "x-ui卸载完成"
+
+    }
+    clear
+    echo -e "
+    vps IPv4：${green}$v4${plain}
+    vps所在地：${green}$c4${plain}
+    x-ui状态：${green}$status1  $status2${plain}
+    yellow "
+        1.安装x-ui(安卓deploy)
+        2.启动x-ui(安卓deploy)
+        3.停止x-ui(安卓deploy)
+        4.卸载x-ui(安卓deploy)
+        5.重置用户名和密码为 admin
+        6.回车取消"
+    read -p "选择序号：" deploy_xui_input
+    case $deploy_xui_input in
+        1) Download_deploy_x-ui && Start_deploy_x-ui ;;
+        2) Start_deploy_x-ui ;;
+        3) Stop_deploy_x-ui ;;
+        4) Uninstall_deploy_x-ui ;;
+        5) /usr/local/x-ui/x-ui setting -username admin -password admin 
+        yellow "用户名和密码已重置为 admin" ;;
+
+        6) exit 0 ;;
+    esac
+
+EOF
+    chmod +x /usr/bin/x-ui
+
     Get_bit
     Download_deploy_x-ui() {
         apt update -y
@@ -248,14 +336,17 @@ Deploy_x-ui() {
         2.启动x-ui(安卓deploy)
         3.停止x-ui(安卓deploy)
         4.卸载x-ui(安卓deploy)
-        5.回车取消"
+        5.重置用户名和密码为 admin
+        6.回车取消"
     read -p "选择序号：" deploy_xui_input
     case $deploy_xui_input in
         1) Download_deploy_x-ui && Start_deploy_x-ui ;;
         2) Start_deploy_x-ui ;;
         3) Stop_deploy_x-ui ;;
         4) Uninstall_deploy_x-ui ;;
-        5) exit 0 ;;
+        5) /usr/local/x-ui/x-ui setting -username admin -password admin 
+        yellow "用户名和密码已重置为 admin" ;;
+        6) exit 0 ;;
     esac
 
 }
